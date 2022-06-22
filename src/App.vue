5e2343524adf9ed8ffc29ea72a61931b89cd1f1c
Marquesjob <patrickmarques.job@hotmail.com>
2022-06-22 16:04:18 -0300

    - template base

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,57 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+<script>
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div>
+        <section class="section">
+            <div class="container">
+                <div class="columns">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+                    <div class="column is-3 lista-de-conversas">
+                        <div class="barra-superior" />
+                    </div>
 
-      <nav>
-        <RouterLink to="/">Home to deploy</RouterLink>
-        <RouterLink to="/about">About to deploy</RouterLink>
-      </nav>
+                    <div class="column conversa-ativa">
+                        <div class="barra-superior">
+                            <span>Usuário</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style>
-@import '@/assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+    .columns {
+         min-height: 800px;
+         box-shadow: 0 3rem 3rem -1rem rgba(10, 10, 10,.2);
+    }
+    
+    .column {
+        padding: 0;
+    }
+    
+    .lista-de-conversas {
+        background: white;
+    }
 
-  font-weight: normal;
-}
+    .conversa-ativa {
+        background: #E5DDD5;
+    }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+    .barra-superior {
+        margin: 0;
+        height: 50px;
+        background: #EDEDED;
+    }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+    .barra-superior span {
+        line-height: 50px;
+        margin-left: 25px;
+        font-weight: 500;
+    }
 
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
